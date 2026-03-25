28a04810c9d827ad48c8cbea94889dce2f0195c2
Kusum dixit <190011112+kusyZenXz@users.noreply.github.com>
2026-03-25 21:01:32 +0530

    Enhance codename generation with random number
    Updated the code to generate a more complex clinical project codename that includes a random number.

--- clinical_code_name_generator_project.py
+++ clinical_code_name_generator_project.py
@@ -1,10 +1,16 @@
-
 print("WELCOME the Clinical Study Code Name Generator")
+
+import random
 
 hospital = input("Enter a hospital or Institude name: ").strip()
 focus = input("Enter the medical speciality or research focus: ").strip()
 
-print(f"Your Cliniccal project codename could be:  {hospital}-{focus}-001")
+hospital_code = hospital[:3].upper()
+focus_code = focus[:3].upper()
+
+number = random.randint(100,999)
+
+print(f"Your Cliniccal project codename could be:  {hospital}-{focus}-2026-{number}")
 
 
 
